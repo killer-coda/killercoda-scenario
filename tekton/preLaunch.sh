4696463e460114353b967b3c28166e38c0787177
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-13 02:25:57 +0800

    Create preLaunch.sh

---
+++ tekton/preLaunch.sh
@@ -0,0 +1,6 @@
+TF_VERSION=1.2.7
+curl -LO https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
+unzip terraform_${TF_VERSION}_linux_amd64.zip
+mv terraform /usr/local/bin
+terraform version   
+terraform -install-autocomplete
