9778e4b8af66a227b1fa7be13b4b9aeb24e55fbb
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-13 02:45:03 +0800

    Update preLaunch.sh

--- tekton/preLaunch.sh
+++ tekton/preLaunch.sh
@@ -1,3 +1,4 @@
+# install terraform
 TF_VERSION=1.2.7
 curl -LO https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
 unzip terraform_${TF_VERSION}_linux_amd64.zip
@@ -5,3 +6,24 @@ mv terraform /usr/local/bin
 terraform version   
 terraform -install-autocomplete
 touch ~/main.tf 
+
+# install k9s
+K9S_VERSION=v0.26.3
+wget https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_x86_64.tar.gz
+tar -xvf k9s_Linux_x86_64.tar.gz
+
+
+# wait fo k8s ready
+while ! kubectl get nodes | grep -w "Ready"; do
+  echo "WAIT FOR NODES READY"
+  sleep 1
+done
+touch /ks/.k8sfinished
+
+# allow pods to run on controlplane
+kubectl taint nodes controlplane node-role.kubernetes.io/master:NoSchedule-
+kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
+
+# mark init finished
+touch /ks/.initfinished
+
