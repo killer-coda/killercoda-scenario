c250116d5b271009802114d11728c798182423e9
hbstarjason2021 <1124484297@qq.com>
2022-08-14 16:56:50 +0000

    add cluster-api files

---
+++ cluster-api/cluster-api.sh
@@ -0,0 +1,23 @@
+
+export CLUSTER_TOPOLOGY=true
+clusterctl init --infrastructure docker
+
+export SERVICE_CIDR=["10.96.0.0/12"]
+export POD_CIDR=["192.168.0.0/16"]
+export SERVICE_DOMAIN="k8s.test"
+
+clusterctl generate cluster capi-quickstart --flavor development \
+  --kubernetes-version v1.24.0 \
+  --control-plane-machine-count=1 \
+  --worker-machine-count=1 \
+  > capi-quickstart.yaml
+
+kubectl apply -f capi-quickstart.yaml
+
+kubectl get cluster
+clusterctl describe cluster capi-quickstart
+
+kubectl get kubeadmcontrolplane
+clusterctl get kubeconfig capi-quickstart > capi-quickstart.kubeconfig
+
+
---
+++ cluster-api/init.sh
@@ -0,0 +1,16 @@
+ apt install net-tools -y
+
+
+# install helm 
+curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+# install kubectl
+curl https://raw.githubusercontent.com/hbstarjason2021/ngrok-k8s/main/install-kubectl.sh | bash
+
+# install kubecolor
+wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.20/kubecolor_0.0.20_Linux_x86_64.tar.gz
+tar zvxf kubecolor_0.0.20_Linux_x86_64.tar.gz && cp kubecolor /usr/local/bin/ && kubecolor version
+
+# install clusterctl
+curl -Lo /usr/local/bin/clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.1.3/clusterctl-linux-amd64 
+chmod +x /usr/local/bin/clusterctl
