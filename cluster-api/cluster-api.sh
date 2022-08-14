2551910ddff933fe985568230b98d848f7d9de5b
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-15 01:01:13 +0800

    Update cluster-api.sh

--- cluster-api/cluster-api.sh
+++ cluster-api/cluster-api.sh
@@ -3,9 +3,16 @@
 export CLUSTER_TOPOLOGY=true
 clusterctl init --infrastructure docker
 
+
+# The list of service CIDR, default ["10.128.0.0/12"]
 export SERVICE_CIDR=["10.96.0.0/12"]
+
+# The list of pod CIDR, default ["192.168.0.0/16"]
 export POD_CIDR=["192.168.0.0/16"]
+
+# The service domain, default "cluster.local"
 export SERVICE_DOMAIN="k8s.test"
+
 
 clusterctl generate cluster capi-quickstart --flavor development \
   --kubernetes-version v1.24.0 \
