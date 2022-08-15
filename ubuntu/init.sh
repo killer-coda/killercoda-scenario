1aa42385c35bba48804914f2057ba01ffc40d652
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-16 00:36:47 +0800

    Update init.sh

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,1 +1,11 @@
- apt install net-tools -y
+apt install net-tools -y
+
+# install helm 
+curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+# install kubectl
+curl https://raw.githubusercontent.com/hbstarjason2021/ngrok-k8s/main/install-kubectl.sh | bash
+
+# install kubecolor
+wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.20/kubecolor_0.0.20_Linux_x86_64.tar.gz
+tar zvxf kubecolor_0.0.20_Linux_x86_64.tar.gz && cp kubecolor /usr/local/bin/ && kubecolor version
