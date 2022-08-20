531af9fd776ed6a522d0885cb41e928286d0122d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-21 01:54:37 +0800

    Update Linkerd.sh

--- Linkerd/Linkerd.sh
+++ Linkerd/Linkerd.sh
@@ -1,3 +1,18 @@
 ### Linkerd
 
+helm repo add linkerd https://helm.linkerd.io/stable
 
+exp=$(date -d '+8760 hour' +"%Y-%m-%dT%H:%M:%SZ")
+  
+
+helm fetch --untar linkerd/linkerd2
+
+helm install linkerd2 \
+  --set-file identityTrustAnchorsPEM=ca.crt \
+  --set-file identity.issuer.tls.crtPEM=issuer.crt \
+  --set-file identity.issuer.tls.keyPEM=issuer.key \
+  --set identity.issuer.crtExpiry=$exp \
+  -f linkerd2/values-ha.yaml \
+  linkerd/linkerd2
+  
+  
