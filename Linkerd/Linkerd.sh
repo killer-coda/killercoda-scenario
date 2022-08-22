33663ebada6053b2aeffd101066df4bc1217b711
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-23 00:27:52 +0800

    Update Linkerd.sh

--- Linkerd/Linkerd.sh
+++ Linkerd/Linkerd.sh
@@ -15,4 +15,37 @@ helm install linkerd2 \
   -f linkerd2/values-ha.yaml \
   linkerd/linkerd2
   
-  
+ 
+ #### https://linkerd.io/2.11/tasks/canary-release/
+ 
+cat <<EOF | kubectl apply -f -
+apiVersion: flagger.app/v1beta1
+kind: Canary
+metadata:
+  name: podinfo
+  namespace: test
+spec:
+  targetRef:
+    apiVersion: apps/v1
+    kind: Deployment
+    name: podinfo
+  service:
+    port: 9898
+  analysis:
+    interval: 10s
+    threshold: 5
+    stepWeight: 10
+    maxWeight: 100
+    metrics:
+    - name: request-success-rate
+      thresholdRange:
+        min: 99
+      interval: 1m
+    - name: request-duration
+      thresholdRange:
+        max: 500
+      interval: 1m
+EOF
+
+kubectl -n test get ev --watch
+
