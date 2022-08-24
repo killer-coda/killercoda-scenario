de5392744ae310dfe1e3b7bb9aed3810ab5f9fea
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-25 00:41:46 +0800

    Update osm-edge.sh

--- osm-edge/osm-edge.sh
+++ osm-edge/osm-edge.sh
@@ -1,4 +1,27 @@
 ### osm-edge
 
 
+system=$(uname -s | tr [:upper:] [:lower:])
+arch=$(dpkg --print-architecture)
+release=v1.1.1
+curl -L https://github.com/flomesh-io/osm-edge/releases/download/${release}/osm-edge-${release}-${system}-${arch}.tar.gz | tar -vxzf -
+./${system}-${arch}/osm version
+cp ./${system}-${arch}/osm /usr/local/bin/
 
+export osm_namespace=osm-system 
+export osm_mesh_name=osm 
+
+osm install \
+    --mesh-name "$osm_mesh_name" \
+    --osm-namespace "$osm_namespace" \
+    --set=osm.enablePermissiveTrafficPolicy=true \
+    --set=osm.deployPrometheus=true \
+    --set=osm.deployGrafana=true \
+    --set=osm.deployJaeger=true \
+    --set=osm.tracing.enable=true \
+    --set=fsm.enabled=true
+    
+ 
+ kubectl get po -n osm-system
+ 
+ 
