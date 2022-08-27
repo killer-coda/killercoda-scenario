45843443edd143fb6a14b02ace4c8c6be235d12c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-27 14:28:51 +0800

    Update init.sh

--- osm-edge/init.sh
+++ osm-edge/init.sh
@@ -8,6 +8,8 @@ curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | 
 curl https://raw.githubusercontent.com/hbstarjason2021/ngrok-k8s/main/install-kubectl.sh | bash
 
 # install kubecolor
-wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.20/kubecolor_0.0.20_Linux_x86_64.tar.gz
-tar zvxf kubecolor_0.0.20_Linux_x86_64.tar.gz && cp kubecolor /usr/local/bin/ && kubecolor version
+#wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.20/kubecolor_0.0.20_Linux_x86_64.tar.gz
+#tar zvxf kubecolor_0.0.20_Linux_x86_64.tar.gz && cp kubecolor /usr/local/bin/ && kubecolor version
 
+curl -L https://github.com/hidetatz/kubecolor/releases/download/v0.0.20/kubecolor_0.0.20_Linux_x86_64.tar.gz | tar -vxzf -
+cp kubecolor /usr/local/bin/ && kubecolor version
