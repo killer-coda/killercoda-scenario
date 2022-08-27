7e8edf935511c2ca42b09dbc9d3c41a818007ea0
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-27 14:44:46 +0800

    Update init.sh

--- osm-edge/init.sh
+++ osm-edge/init.sh
@@ -13,3 +13,7 @@ curl https://raw.githubusercontent.com/hbstarjason2021/ngrok-k8s/main/install-ku
 
 curl -L https://github.com/hidetatz/kubecolor/releases/download/v0.0.20/kubecolor_0.0.20_Linux_x86_64.tar.gz | tar -vxzf -
 cp kubecolor /usr/local/bin/ && kubecolor version
+
+
+kubectl apply -f  https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.22/deploy/local-path-storage.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
