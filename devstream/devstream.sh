c802ea27887c1342d10bc34e0b720ce125381e91
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-09-23 00:56:53 +0800

    Update devstream.sh

--- devstream/devstream.sh
+++ devstream/devstream.sh
@@ -1,6 +1,14 @@
 ### https://docs.devstream.io/en/latest/quickstart/
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/devstream-io/devstream/main/hack/quick-start/quickstart.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/devstream-io/devstream/main/hack/quick-start/quickstart.sh)"     
+
+export GITHUB_USER="<YOUR_GITHUB_USER_NAME_HERE>"     
+export GITHUB_TOKEN="<YOUR_GITHUB_PERSONAL_ACCESS_TOKEN_HERE>"    
+export DOCKERHUB_USERNAME="<YOUR_DOCKER_HUB_USER_NAME_HERE>"     
+ 
+sed -i "s@YOUR_GITHUB_USERNAME_CASE_SENSITIVE@${GITHUB_USER}@g" quickstart.yaml    
+sed -i "s@YOUR_DOCKER_USERNAME@${DOCKERHUB_USERNAME}@g" quickstart.yaml      
+
 
 ./dtm init -f quickstart.yaml
 
