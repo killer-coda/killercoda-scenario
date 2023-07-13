5fd9d2dd5974025b13d40a81cbb71ed9fc2c1c2d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-07-13 21:56:02 +0800

    Update init.sh

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -12,3 +12,11 @@ tar zvxf kubecolor_0.0.20_Linux_x86_64.tar.gz && cp kubecolor /usr/local/bin/ &&
 
 ## mkdir -m 750 ~/.kube
 ## cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+# install terraform
+TF_VERSION="1.5.2"
+curl -LO https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
+unzip terraform_${TF_VERSION}_linux_amd64.zip
+mv terraform /usr/local/bin
+terraform version   
+terraform -install-autocomplete
