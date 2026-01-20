11a47e1359f80e1048218c7f0a4e89326050d1b6
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2026-01-20 14:56:57 +0800

    Update init.sh

--- lianghua/init.sh
+++ lianghua/init.sh
@@ -1,3 +1,8 @@
 ###
+apt install net-tools  jq -y
 
 pip install requests pandas  --break-system-packages
+
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh && sudo -E bash nodesource_setup.sh
+
+sudo apt-get install -y nodejs
